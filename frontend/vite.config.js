c9039c3d4651cecd7bdde17d8f29f48738da25b2
VGLKSathsara <lanishkakawenn926@gmail.com>
2026-03-30 07:59:24 +0530

    reclean

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:4000',
-    },
-  },
+  server: { port: 5173 },
 })
